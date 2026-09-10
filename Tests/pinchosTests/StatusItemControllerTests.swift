12dea4cb7ee80f8048a6ee2877a04b22c9aa1978
Douglas Jarquin <8209+douglasjarquin@users.noreply.github.com>
2026-09-10 16:39:56 -0400

    test: accept concurrent lifecycle preparation order

--- Tests/pinchosTests/StatusItemControllerTests.swift
+++ Tests/pinchosTests/StatusItemControllerTests.swift
@@ -559,9 +559,11 @@ final class StatusItemControllerTests: XCTestCase {
         XCTAssertTrue(beta !== factory.created[2])
         XCTAssertEqual(factory.created[2].config.name, "gamma")
         XCTAssertFalse(factory.created[2].initiallyVisible)
-        XCTAssertEqual(factory.eventLog.events, [
+        XCTAssertEqual(Set(factory.eventLog.events.prefix(2)), [
             "prepare-update:alpha",
-            "prepare-removal:beta",
+            "prepare-removal:beta"
+        ])
+        XCTAssertEqual(factory.eventLog.events.suffix(3), [
             "commit-removal:beta",
             "commit-update:alpha",
             "activate:gamma"
